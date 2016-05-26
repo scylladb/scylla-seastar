b80564dff1a3a2f0e1319a792933c860780adbc2
Avi Kivity <avi@scylladb.com>
2016-05-26 11:42:19 +0300

    reactor: advertise the logging_failures metric as a DERIVE counter
    Since it is under total_operations, it must be of type DERIVE.  Prometheus
    checks it and gets confused.
    
    (cherry picked from commit 8bfbb1a009f5a92924f2cc77a67e0be107e9c9c8)

--- core/reactor.cc
+++ core/reactor.cc
@@ -1481,7 +1481,7 @@ reactor::register_collectd_metrics() {
                 scollectd::type_instance_id("reactor",
                     scollectd::per_cpu_plugin_instance,
                     "total_operations", "logging_failures"),
-                scollectd::make_typed(scollectd::data_type::GAUGE,
+                scollectd::make_typed(scollectd::data_type::DERIVE,
                         [] { return logging_failures; })
             ),
     } };
