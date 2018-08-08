7d1edd21c0f3463c1f092b56211c9a61f5788982
Glauber Costa <glauber@scylladb.com>
2018-08-08 13:40:29 -0400

    reactor: switch indentation
    Signed-off-by: Glauber Costa <glauber@scylladb.com>
    Message-Id: <20180808174029.12521-5-glauber@scylladb.com>
    (cherry picked from commit 8ad870fc49b13d2657543ff09dc181634865fd05)

--- core/reactor.cc
+++ core/reactor.cc
@@ -4010,14 +4010,14 @@ public:
 
         cfg.capacity = per_io_queue(_capacity);
         if (cfg.capacity == std::numeric_limits<unsigned>::max()) {
-        cfg.disk_bytes_write_to_read_multiplier = (io_queue::read_request_base_count * _read_bytes_rate) / _write_bytes_rate;
-        cfg.disk_req_write_to_read_multiplier = (io_queue::read_request_base_count * _read_req_rate) / _write_req_rate;
-        cfg.max_req_count = max_bandwidth == std::numeric_limits<uint64_t>::max()
-            ? std::numeric_limits<unsigned>::max()
-            : io_queue::read_request_base_count * per_io_queue(max_iops * _latency_goal.count());
-        cfg.max_bytes_count = max_iops == std::numeric_limits<uint64_t>::max()
-            ? std::numeric_limits<unsigned>::max()
-            : io_queue::read_request_base_count * per_io_queue(max_bandwidth * _latency_goal.count());
+            cfg.disk_bytes_write_to_read_multiplier = (io_queue::read_request_base_count * _read_bytes_rate) / _write_bytes_rate;
+            cfg.disk_req_write_to_read_multiplier = (io_queue::read_request_base_count * _read_req_rate) / _write_req_rate;
+            cfg.max_req_count = max_bandwidth == std::numeric_limits<uint64_t>::max()
+                ? std::numeric_limits<unsigned>::max()
+                : io_queue::read_request_base_count * per_io_queue(max_iops * _latency_goal.count());
+            cfg.max_bytes_count = max_iops == std::numeric_limits<uint64_t>::max()
+                ? std::numeric_limits<unsigned>::max()
+                : io_queue::read_request_base_count * per_io_queue(max_bandwidth * _latency_goal.count());
         } else {
             cfg.disk_bytes_write_to_read_multiplier = 1;
             cfg.disk_req_write_to_read_multiplier = 1;
