62fd873d09fbe63db53e212f614aa0c31425b7d3
Botond Dénes <bdenes@scylladb.com>
2023-03-29 13:55:36 +0300

    core/on_internal_error: always log error with backtrace
    For some reason the backtrace was only logged when
    `abort_on_internal_error` was set to true, otherwise the backtrace was
    only attached to the exception thrown but not the log message.
    This doesn't makes sense and it means that if the exception is not
    printed (correctly) the backtrace is lost completely.
    This patch fixes this and makes sure the error message is logged with a
    backtrace, regardless of what `abort_on_internal_error` is set to.
    No performance degradation is expected from this, internal error should
    be *very* rare and once they happen, the performance of the affected
    code path is not interesting anymore.
    
    Closes #1584
    
    (cherry picked from commit a207df048fd78c6c5f13f306fc68b8dd50965fbe)

--- src/core/on_internal_error.cc
+++ src/core/on_internal_error.cc
@@ -33,13 +33,14 @@ bool seastar::set_abort_on_internal_error(bool do_abort) noexcept {
     return abort_on_internal_error.exchange(do_abort);
 }
 
-static void log_error_and_backtrace(logger& logger, std::string_view msg) noexcept {
+template <typename Message>
+static void log_error_and_backtrace(logger& logger, const Message& msg) noexcept {
     logger.error("{}, at: {}", msg, current_backtrace());
 }
 
 void seastar::on_internal_error(logger& logger, std::string_view msg) {
+    log_error_and_backtrace(logger, msg);
     if (abort_on_internal_error.load()) {
-        log_error_and_backtrace(logger, msg);
         abort();
     } else {
         throw_with_backtrace<std::runtime_error>(std::string(msg));
@@ -47,8 +48,8 @@ void seastar::on_internal_error(logger& logger, std::string_view msg) {
 }
 
 void seastar::on_internal_error(logger& logger, std::exception_ptr ex) {
+    log_error_and_backtrace(logger, ex);
     if (abort_on_internal_error.load()) {
-        logger.error("{}", ex);
         abort();
     } else {
         std::rethrow_exception(std::move(ex));
